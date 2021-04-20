f8d25c1ceed3c0fc9064389b930157110ead47fb
ericaphillips <ericaphill@gmail.com>
2021-04-20 10:26:24 -0500

    done

--- practice.js
+++ practice.js
@@ -42,3 +42,20 @@ const calculateBmi = (weight, height) => {
   const test = calculateBmi(200, 74)
   
   console.log(test)
+
+  // Your function takes two arguments:
+
+// current father's age (years)
+
+// current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+const kidAge = 5
+const dadAge = 31
+
+const twiceAsOld = (d, s) => Math.abs(d - 2 * s);
+
+const test = twiceAsOld(31, 10)
+
+console.log(test)
