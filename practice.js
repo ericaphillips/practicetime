cbf8a2dd820647007d1274d4a7b93f78e587488c
ericaphillips <ericaphill@gmail.com>
2021-04-26 11:07:25 -0500

    done

--- practice.js
+++ practice.js
@@ -82,3 +82,31 @@ const temperatureCheck = (x) => {
   const test = temperatureCheck(300)
   
   console.log(test)
+
+  function firstRepeating(str)
+{
+     
+    // Creates an empty hashset
+    let h = new Set();
+     
+     
+    // Traverse the input array from left to right
+    for(let i = 0; i <= str.length -1; i++)
+    {
+        let c = str[i];
+ 
+        // If element is already in hash
+        // set, update x and then break
+        if (h.has(c))
+            return c;
+             
+        // Else add element to hash set
+        else
+            h.add(c);
+    }
+    return '\0';
+}
+ 
+// Driver code
+let str = "gaass";
+console.log(firstRepeating(str));
